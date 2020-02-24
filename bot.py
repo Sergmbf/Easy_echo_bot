86c9069382a0c18caa5e9f36b90e1d4816eb27cf
Sergey Petrov <serg4309@yandex.ru>
2020-02-25 01:23:44 +0300

    Easy echo_bot_comm5

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-
 import logging
 import setting
+
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
 
 
 def greet_user(bot, update):
